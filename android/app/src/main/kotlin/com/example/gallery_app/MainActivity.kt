cf3ef2c1f5f06d971a6780ec17cb7db0b24a0ecd
Ghaith200 <abdullahghiath2003@gmail.com>
2024-09-12 18:14:13 +0300

    Startup_point

---
+++ android/app/src/main/kotlin/com/example/gallery_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gallery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import network_info_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+}
